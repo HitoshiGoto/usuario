568fa842e2e669fae72d25be0169bde9b0701eec
Hitoshi <hitoshigoto@outlook.com>
2026-01-28 03:49:45 -0300

    adicionados classes de security

---
+++ src/main/java/com/hitoshi/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.hitoshi.usuario.infrastructure.repository;
+
+import com.hitoshi.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/hitoshi/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.hitoshi.usuario.infrastructure.repository;
+
+import com.hitoshi.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
